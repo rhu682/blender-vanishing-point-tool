0f164723216d60acebb290be181d9621a8611149
rhu682 <bhuang@g.hmc.edu>
2023-11-18 11:41:53 -0800

    added TODO skeleton

--- testscript.py
+++ testscript.py
@@ -1,7 +1,13 @@
 # Scripts to test go here.
-
 import bpy
 
 scene = bpy.context.scene
-for obj in scene.objects:
-    obj.location.x += 1.0
+''' User must select first the image, then the aligning plane, and nothing 
+else, and then trigger this script.'''
+
+# TODO: Get image and plane data
+
+# TODO: trigger a dummy script
+
+# TODO: Make new camera, give it a dummy position/rotation/perspective
+# TODO: Move image to be head-on with camera
